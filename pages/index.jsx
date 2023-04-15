eb94587a138bda01dc9b996e56d9be7812394ad2
hlmoore6 <hlmoore6@mail.fhsu.edu>
2023-04-15 14:02:11 -0500

    Changed the index page. Created code directory

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 
+const Index = () => {
 
-import Nav from '../components/nav'
-import Codearea from '../components/codearea'
-import Playerstatus from '../components/playerstatus'
-import Problem from '../components/problem'
-
-import styles from "./index.module.css"
-
-const inter = Inter({ subsets: ['latin'] })
-
-const Home = () => {
-  return (
-    <>
-      <div class={styles.container}>
-        <div class={styles.codearea}>
-          <Codearea/>
-        </div>
-        <div class={styles.problem}>
-          <Problem/>
-        </div>
-        <div class={styles.playerstatus}>
-          <Playerstatus/>
-        </div>
-      </div>
-    </>
-  )
+    return (
+        <>
+        </>
+    );
 }
 
-export default Home;
+export default Index;
