d7abf73ca5b2c42a2599fbd3a7db02984f892a6b
hlmoore6 <hlmoore6@mail.fhsu.edu>
2023-04-15 16:50:41 -0500

    Added pages utils classes for global storage

---
+++ pages/util/player.js
@@ -0,0 +1,3 @@
+import { Player } from "../api/utils/players";
+
+export const player = new Player();
---
+++ pages/util/problem.js
@@ -0,0 +1,12 @@
+export const problem = {
+        uuid: "some uuid",
+        name: "name",
+        description: "desc",
+        tags: ["name", "other tags"],
+        input_examples: "",
+        output_examples: "",
+        solution: "",
+        points: "",
+        difficulty: 0,
+        examples: []
+}
