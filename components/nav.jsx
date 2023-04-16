8cc6e6778c25c936f2b7932452a079a7cca14974
hlmoore6 <hlmoore6@mail.fhsu.edu>
2023-04-15 20:03:01 -0500

    Fixed small className issue

--- components/nav.jsx
+++ components/nav.jsx
@@ -5,11 +5,7 @@ const Nav = () => {
 
     return (
         <nav className={styles.nav}>
-<<<<<<< HEAD
-                <Link href="#" className={styles.navLink}>Nav Bar</Link>
-=======
-            <Link href="#" class={styles.navLink}>Nav Bar</Link>
->>>>>>> 61a90b0d481c27de2975ec1a3c9c7f490ba85a81
+            <Link href="#" className={styles.navLink}>Nav Bar</Link>
         </nav>
     );
 }
